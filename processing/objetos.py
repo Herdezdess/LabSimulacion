46d5039f88b2625f53fcd80f7f7ea2a977c01418
Herdezdess <160191503+Herdezdess@users.noreply.github.com>
2025-05-10 04:05:35 -0600

    Update objetos.py

--- processing/objetos.py
+++ processing/objetos.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import numpy as np
 import random
-from distribuciones import Distribuciones
+from processing.distribuciones import Distribuciones
 
 class Objeto:
   def __init__(self, tipo, config):
