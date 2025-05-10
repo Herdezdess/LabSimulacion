6893faad3bad5899f7fed45d982eb3f5d1fa11ee
Herdezdess <160191503+Herdezdess@users.noreply.github.com>
2025-05-10 03:52:12 -0600

    Update distribuciones.py

--- processing/distribuciones.py
+++ processing/distribuciones.py
@@ -1,28 +1,16 @@
+#distribuciones.py
 import numpy as np
 
-class Distribucion():
+class Distribuciones:
   def _init_(self, N:int=None):
-    self.N_ = N #aquí definimos que N sean los elementos que se generan
-  #para el proyecto se nos pide una distribución normal y una uniforme
-  def distribucion_gaussiana(self, media:float = 0.0, desviacion:float=0.0):
+    self.N_ = N
+  @staticmethod
+  def gaussiana(self, media:float = 0.0, desviacion:float=0.0):
     dist = np.random.normal(media, desviacion, self._N)
     return dist
-
-  def distribucion_uniforme(self, media:float = 0.0, desviacion:float = 0.0):
-    dist = np.random.normal(media, desviacion, self._N)
+  @staticmethod
+  def uniforme(valor_minimo:float = 0.0, valor_maximo:float = 0.0):
+    dist = np.random.uniform(valor_minimo, valor_maximo, self._N)
     return dist
-  
-  #set nosdevuelve datos aleatorios, dependiendo de la distribución
-  def set(self, valor_inicial:float=None, valor_final:float=None, distribucion:str=""):
-        try:
-            if distribucion=="normal":
-                dist = self._distribucion_gaussiana(valor_inicial, valor_final)
-            elif distribucion=="uniform":
-                dist = self._distribucion_uniforme(valor_inicial, valor_final)
-            else:
-                raise Exception("No hay distribucion")
-            return dist
-        except Exception as e:
-            print(f"Error: {e}")
 
     
