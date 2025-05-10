d58381ebc3413827c0835eaa1b4b0ccd31239f7a
Herdezdess <160191503+Herdezdess@users.noreply.github.com>
2025-05-10 02:08:04 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-a
+from processing.simulation import simulation
+
+def main()#se crea la simulacion y se ejecuta
+  simulacion = Simulation() 
+  simulacion.run()
+
+if _name_ == "_main_": #ejecucion del archivo
+  main()
