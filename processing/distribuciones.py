489d5365a3bb497e7fb47a39bf10db910a1d6465
Herdezdess <160191503+Herdezdess@users.noreply.github.com>
2025-05-10 00:27:06 -0600

    Update and rename distributions.py to distribuciones.py

--- processing/distribuciones.py
+++ processing/distribuciones.py
@@ -4,7 +4,7 @@ class Distribucion():
   def _init_(self, N:int=None):
     self.N_ = N #aquí definimos que N sean los elementos que se generan
   #para el proyecto se nos pide una distribución normal y una uniforme
-  def distribucion_normal(self, media:float = 0.0, desviacion:float=0.0):
+  def distribucion_gaussiana(self, media:float = 0.0, desviacion:float=0.0):
     dist = np.random.normal(media, desviacion, self._N)
     return dist
 
@@ -16,9 +16,9 @@ class Distribucion():
   def set(self, valor_inicial:float=None, valor_final:float=None, distribucion:str=""):
         try:
             if distribucion=="normal":
-                dist = self._normal_distribution(valor_inicial, valor_final)
+                dist = self._distribucion_gaussiana(valor_inicial, valor_final)
             elif distribucion=="uniform":
-                dist = self._uniform_distribution(valor_inicial, valor_final)
+                dist = self._distribucion_uniforme(valor_inicial, valor_final)
             else:
                 raise Exception("No hay distribucion")
             return dist
