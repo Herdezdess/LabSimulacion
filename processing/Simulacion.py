032245823b20d9a8ff95b6852ab449a1ac95d6ca
Herdezdess <160191503+Herdezdess@users.noreply.github.com>
2025-05-10 22:59:14 -0600

    Update Simulacion.py

--- processing/Simulacion.py
+++ processing/Simulacion.py
@@ -1,61 +1,52 @@
-import pygame
+# processing/simulacion.py
+
 import math
-from processing.objetos import Pelotas
+import config
+from processing.objeto import Particula  # Importamos la clase Particula
 
-class DIBUJITOS:
-    def __init__(self, objetos, config):
-        self.objetos = objetos
-        self.config= config
-        self.gravedad = config['simulacion']['gravedad']
-        self.tiempo_simulacion = config['simulacion']['tiempo_simulacion']
-        self.ancho = config['simulacion']['ancho_espacio']
-        self.alto = config['simulacion']['alto_espacio']
-        self.radio_interaccion = config['simulacion']['radio_interaccion']
+# Función para simular gravedad
+def aplicar_gravedad(particulas):
+    for i, a in enumerate(particulas):
+        for j, b in enumerate(particulas):
+            if i == j:
+                continue
+            dx = b.x - a.x
+            dy = b.y - a.y
+            distancia = math.hypot(dx, dy)
+            if distancia == 0:
+                continue
+            fuerza = config.G * a.masa * b.masa / distancia**2
+            ax = fuerza * dx / distancia / a.masa
+            ay = fuerza * dy / distancia / a.masa
+            a.vx += ax
+            a.vy += ay
 
-    def gravitacion(self):
-        # Interacción gravitacional simple entre objetos
-        for objeto_1 in self.objetos:
-            for objeto_2 in self.objetos:
-                if objeto_1 != objeto_2:
-                    dx = objeto_2.x - objeto_1.x
-                    dy = objeto_2.y - objeto_1.y
-                    distancia = math.sqrt(dx**2 + dy**2)
-                    if distancia < self.radio_interaccion:
-                        fuerza = self.gravedad * objeto_1.masa * objeto_2.masa / distancia**2
-                        objeto_1.velocidad_x += fuerza * dx / distancia
-                        objeto_1.velocidad_y += fuerza * dy / distancia
-
-    def actualizar_posiciones(self):
-        for objeto in self.objetos:
-            objeto.x += objeto.velocidad_x
-            objeto.y += objeto.velocidad_y
-
-    def colision(self):
-      nuevo_objetos = []  # Lista de objetos nuevos
-      self.radio_colision = self.config['simulacion']['radio_colision']
-      for i, objeto_1 in enumerate(self.objetos):
-        for j, objeto_2 in enumerate(self.objetos):
-            if i != j:
-                dx = objeto_2.x - objeto_1.x
-                dy = objeto_2.y - objeto_1.y
-                distancia = math.sqrt(dx**2 + dy**2)
-                if distancia < self.radio_colision:
-                    masa_total = objeto_1.masa + objeto_2.masa
-                    xn = (objeto_1.x * objeto_1.masa + objeto_2.x * objeto_2.masa) / masa_total
-                    yn = (objeto_1.y * objeto_1.masa + objeto_2.y * objeto_2.masa) / masa_total
-                    vxn = (objeto_1.velocidad_x * objeto_1.masa + objeto_2.velocidad_x * objeto_2.masa) / masa_total
-                    vyn = (objeto_1.velocidad_y * objeto_1.masa + objeto_2.velocidad_y * objeto_2.masa) / masa_total
-                    radio_nuevo = (objeto_1.radio + objeto_2.radio) / 2
-                    tipo = objeto_1.tipo  # Tipo del objeto 1
-
-                    # Asegúrate de pasar la configuración correctamente
-                    nuevo_objeto = Pelotas(tipo, self.config)  # Pasa self.config a Pelotas
-                    nuevo_objetos.append(nuevo_objeto)
-                    return True
-      return False
-
-
-    def simular(self):
-        self.gravitacion()
-        self.actualizar_posiciones()
-        self.colision()
+# Función para fusionar partículas cercanas
+def fusionar(particulas):
+    nuevas = []
+    skip = set()
+    for i in range(len(particulas)):
+        if i in skip:
+            continue
+        a = particulas[i]
+        for j in range(i + 1, len(particulas)):
+            if j in skip:
+                continue
+            b = particulas[j]
+            dx = b.x - a.x
+            dy = b.y - a.y
+            distancia = math.hypot(dx, dy)
+            if distancia < config.FUSION_DIST:
+                # Fusionar: la nueva masa es la suma de las masas de las dos partículas
+                nueva_masa = a.masa + b.masa
+                nueva_x = (a.x * a.masa + b.x * b.masa) / nueva_masa
+                nueva_y = (a.y * a.masa + b.y * b.masa) / nueva_masa
+                nueva_vx = (a.vx * a.masa + b.vx * b.masa) / nueva_masa
+                nueva_vy = (a.vy * a.masa + b.vy * b.masa) / nueva_masa
+                nuevas.append(Particula(nueva_x, nueva_y, nueva_masa, nueva_vx, nueva_vy))
+                skip.add(i)
+                skip.add(j)
+                break
+        else:
+            nuevas.append(a)
+    return nuevas
