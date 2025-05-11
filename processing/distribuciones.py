caba7f90872c0d90c412ae2a03802651f3411ca7
Herdezdess <160191503+Herdezdess@users.noreply.github.com>
2025-05-10 23:00:02 -0600

    Update distribuciones.py

--- processing/distribuciones.py
+++ processing/distribuciones.py
@@ -1,17 +1,11 @@
-import numpy as np
 import random
 
+def generar_masa_gaussiana(media=10, desviacion=2):
+    """Genera una masa con distribución normal."""
+    return max(1, random.gauss(media, desviacion))  # evitar masas negativas o cero
 
-class Distribuciones:
-  def __init__(self, config=None):
-      if config is not None:
-         self.config = config
-      else:
-         with open("configuration.yaml", "r") as file:
-              self.config = yaml.safe_load(file)
-      
-  def gaussiana(self, media, desviacion, N):
-    return np.random.normal(media,desviacion,N)
-  
-  def uniforme(self, valor_minimo, valor_maximo, N):
-    return [random.uniform(valor_minimo, valor_maximo) for _ in range(N)]
+def generar_posicion_uniforme(ancho, alto, margen=100):
+    """Genera una posición aleatoria uniforme dentro de los límites del espacio."""
+    x = random.uniform(margen, ancho - margen)
+    y = random.uniform(margen, alto - margen)
+    return x, y
